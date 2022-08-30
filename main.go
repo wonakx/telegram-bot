3f270a08dd0b33062f9e6aae52ecec42234c324a
wonakx <wonakx@gmail.com>
2022-08-31 03:17:15 +0900

    File Log 에 저장하도록 수정

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
+	"os/signal"
 	_ "telegram-bot/bot"
 	_ "telegram-bot/config"
 	_ "telegram-bot/transmission"
-	"time"
 )
 
 func main() {
@@ -23,5 +24,27 @@ func main() {
 
 	log.SetOutput(logFile)
 
-	time.Sleep(999999 * time.Hour)
+	// load command line arguments
+	name := flag.String("name", "world", "name to print")
+	flag.Parse()
+
+	log.Printf("Starting sleepservice for %s", *name)
+
+	// setup signal catching
+	sigs := make(chan os.Signal, 1)
+
+	// catch all signals since not explicitly listing
+	signal.Notify(sigs)
+	//signal.Notify(sigs,syscall.SIGQUIT)
+
+	// method invoked upon seeing signal
+	s := <-sigs
+	log.Printf("RECEIVED SIGNAL: %s", s)
+	AppCleanup()
+	os.Exit(1)
+
+}
+
+func AppCleanup() {
+	log.Println("CLEANUP APP BEFORE EXIT!!!")
 }
