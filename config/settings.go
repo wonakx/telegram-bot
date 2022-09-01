ebb4cb94a60445544a6bba1b061db949c079af32
wonakx <wonakx@gmail.com>
2022-09-02 00:04:18 +0900

    토렌트 파일 추가 명령어로 변경

--- config/settings.go
+++ config/settings.go
@@ -1,12 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"strconv"
-	"telegram-bot/logwrapper"
 )
-
-var log = logwrapper.NewLogger()
 
 var ChatId int64
 var Token string
@@ -32,26 +30,16 @@ func init() {
 	SubtitleExts = []string{".srt", ".smi", ".SRT", ".SMI"}
 	TransmissionCommands = []string{"list", "add", "del"}
 
-	log.Info("subtitleExts", SubtitleExts)
-	log.Info("transmissionCommands", TransmissionCommands)
-
 	CommonFilePath = os.Getenv("COMMON_FILE_PATH")
 	SubtitleFilePath = os.Getenv("SUB_FILE_PATH")
 	TorrentFilePath = os.Getenv("TORRENT_FILE_PATH")
 
-	log.Info("commonFilePath", CommonFilePath)
-	log.Info("subtitleFilePath", SubtitleFilePath)
-	log.Info("torrentFilePath", TorrentFilePath)
-
 	Token = os.Getenv("TG_TOKEN")
 	ChatIdStr := os.Getenv("TG_CHAT_ID")
 
-	log.Info("TG_TOKEN:", Token)
-	log.Info("TG_CHAI_ID:", ChatIdStr)
-
 	chatIdInt, chatIdErr := strconv.Atoi(ChatIdStr)
 	if chatIdErr != nil {
-		log.Error(chatIdErr)
+		fmt.Print(chatIdErr)
 	}
 	ChatId = int64(chatIdInt)
 
@@ -59,7 +47,4 @@ func init() {
 	TmUsername = os.Getenv("TRANSMISSION_USERNAME")
 	TmPassword = os.Getenv("TRANSMISSION_PASSWORD")
 
-	log.Info("TransmissionPort:", TmPort)
-	log.Info("TransmissionUsername:", TmUsername)
-	log.Info("TransmissionPassword:", TmPassword)
 }
