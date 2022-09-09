9f8f8549331f2ae9bc61039e97fb1fe447cb280a
wonakx <wonakx@gmail.com>
2022-09-09 13:31:49 +0900

    Transmission 라이브러리 도입

--- logwrapper/logwrapper.go
+++ logwrapper/logwrapper.go
@@ -23,13 +23,15 @@ func NewLogger() *StandardLogger {
 	var logFilePath = config.LogFilePath
 
 	var logFile *os.File
-	if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
+	if _, err := os.Stat(logFilePath); err == nil {
 		logFile, _ = os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	} else {
+	} else if os.IsNotExist(err) {
 		logFile, _ = os.Create(logFilePath)
 		if err != nil {
 			log.Fatalln(err)
 		}
+	} else {
+		log.Println(err)
 	}
 
 	baseLogger.Out = logFile
